e4eee0e8bfaeb702e9ee04cc2ccee5d3cbea4061
dev <sups56918@gmail.com>
2024-10-06 07:06:02 +0700

    Them new fragment

--- app/src/main/java/com/example/navigation/FifthFragment.kt
+++ app/src/main/java/com/example/navigation/FifthFragment.kt
@@ -1,13 +1,10 @@
 package com.example.navigation
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.findNavController
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [Fourth.newInstance] factory method to
+ * Use the [FifthFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class Fourth : Fragment() {
+class FifthFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -37,9 +34,7 @@ class Fourth : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_fourth, container, false)
-        view.findViewById<Button>(R.id.btnFifth).setOnClickListener { view.findNavController().navigate(R.id.action_fourth_to_fifthFragment) }
-        return view
+        return inflater.inflate(R.layout.fragment_fifth, container, false)
     }
 
     companion object {
@@ -49,12 +44,12 @@ class Fourth : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment Fourth.
+         * @return A new instance of fragment FifthFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            Fourth().apply {
+            FifthFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
